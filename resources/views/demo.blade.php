45ec7224a7c3ee2b05662e2ec595c7289f192ab5
catlookatyou <woozoo6834@gmail.com>
2021-06-09 20:18:37 +0800

    tailwind

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -8,21 +8,43 @@
 
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="antialiased">
+    <body class="font-sans">
         <div id="app">
-            <!-- 路由匹配到的组件将渲染在这里 -->
-            <router-view></router-view>
+            <div class="container px-8">
+                <main class="flex">
+                    <!-- 侧边栏（导航菜单） -->
+                    <aside class="w-1/3 pt-8">
+                        <section class="mb-10">
+                            <ul class="list-reset">
+                                <h1 class="mb-4">
+                                    catlookatyou
+                                </h1>
+                                <li class="leading-loose">
+                                    <router-link to="/">Home</router-link>
+                                </li>
+                                <li class="leading-loose">
+                                    <router-link :to="{ name: 'post', params: { name: 'cat1' }}">cat1</router-link>
+                                </li>
+                                <li class="leading-loose">
+                                    <router-link :to="{ name: 'post', params: { name: 'cat2' }}">cat2</router-link>
+                                </li>
+                                <li class="leading-loose">
+                                    <router-link to="/about">About</router-link>
+                                </li>
+                            </ul>
+                        </section>
+                    </aside>
 
-            <hr>
-
-            <!-- 使用 router-link 组件来导航 -->
-            <!-- 通过传入 `to` 属性指定链接 -->
-            <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-            <router-link to="/">Home</router-link>
-             <!-- 传递路由参数到命名路由 -->
-            <router-link :to="{ name: 'post', params: { name: 'helloworld' }}">helloworld</router-link>
-            <router-link :to="{ name: 'post', params: { name: 'cat' }}">cat</router-link>
-            <router-link to="/about">About</router-link>
+                    <!-- 主体内容 -->
+                    <div class="primary pt-20">
+                        <!-- 路由匹配到的组件将渲染在这里 -->
+                        <router-view></router-view>
+                    </div>
+                    
+                    <!-- 底部内容 -->
+                    <footer></footer>
+                </main>
+            </div>
         </div>
 
         <script src="{{ asset('/js/app.js') }}"></script>
