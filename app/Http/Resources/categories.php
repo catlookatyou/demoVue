14bf302840365a765ed7508a338ad7e4d97df858
catlookatyou <woozoo6834@gmail.com>
2021-06-21 16:28:35 +0800

    Category resource

--- app/Http/Resources/categories.php
+++ app/Http/Resources/categories.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class categories extends ResourceCollection
+class Categories extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
